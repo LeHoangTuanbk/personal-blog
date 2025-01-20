d36077343567f762389b42fafe88b036b3713f40
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-01-21 00:38:48 +0900

    Refactor: Code

--- src/features/login/ui/login-form.tsx
+++ src/features/login/ui/login-form.tsx
@@ -11,43 +11,33 @@ import {
   Text,
   VStack,
 } from '@chakra-ui/react';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import {
+  UseFormRegister,
+  FieldErrors,
+  UseFormHandleSubmit,
+} from 'react-hook-form';
 
-import { useLoginForm } from '@features/login/api';
 import { LoginSchema } from '@features/login/api/schema';
-import { supabaseClient } from '@shared/api/supabase-client';
-import { paths } from '@shared/config/paths';
-import { useToast } from '@shared/ui/components/toast-factory';
-export const LoginForm = () => {
-  const {
-    handleSubmit,
-    register,
-    formState: { errors },
-  } = useLoginForm();
-  const { errorToast, successToast } = useToast();
-  const [isLoading, setIsLoading] = useState(false);
-  const [showPassword, setShowPassword] = useState(false);
-  const navigate = useNavigate();
-  const onSubmit = async (data: LoginSchema) => {
-    setIsLoading(true);
-    const { data: signInData, error: signInError } =
-      await supabaseClient.auth.signInWithPassword({
-        email: data.email,
-        password: data.password,
-      });
-    if (signInError) {
-      errorToast(signInError.message);
-      setIsLoading(false);
-      return;
-    }
-    if (signInData) {
-      successToast('Login successful');
-      navigate(paths.admin.home);
-      return;
-    }
-    setIsLoading(false);
-  };
+
+type LoginFormProps = {
+  register: UseFormRegister<LoginSchema>;
+  errors: FieldErrors<LoginSchema>;
+  isLoading: boolean;
+  showPassword: boolean;
+  setShowPassword: (show: boolean) => void;
+  handleSubmit: UseFormHandleSubmit<LoginSchema>;
+  onSubmit: (data: LoginSchema) => Promise<void>;
+};
+
+export const LoginForm = ({
+  register,
+  errors,
+  isLoading,
+  showPassword,
+  setShowPassword,
+  handleSubmit,
+  onSubmit,
+}: LoginFormProps) => {
   return (
     <Box
       w="md"
