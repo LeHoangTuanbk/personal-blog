9deb167a7b523c19a6318e00068c49238a560e88
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-01-21 22:47:22 +0900

    Update: Add logout

--- src/pages/admin/dashboard/ui/admin-dashboard.tsx
+++ src/pages/admin/dashboard/ui/admin-dashboard.tsx
@@ -1,3 +1,20 @@
+import { Box, Text, Button } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+
+import { supabaseClient } from '@shared/api/supabase-client';
+import { useToast } from '@shared/ui/components/toast-factory';
 export const AdminDashboard = () => {
-  return <div>Admin Dashboard</div>;
+  const navigate = useNavigate();
+  const { successToast } = useToast();
+  const handleLogout = async () => {
+    await supabaseClient.auth.signOut();
+    successToast('Logged out successfully');
+    navigate('/login');
+  };
+  return (
+    <Box>
+      <Text>Admin Dashboard</Text>
+      <Button onClick={handleLogout}>Logout</Button>
+    </Box>
+  );
 };
