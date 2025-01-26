d3ad9661a2bf3289bde936c47fb7be3d4c80c6e4
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-01-26 15:44:49 +0900

    Update: Add post form

--- src/pages/admin/edit-post/ui/admin-edit-post.tsx
+++ src/pages/admin/edit-post/ui/admin-edit-post.tsx
@@ -1,3 +1,14 @@
+import { Text, VStack } from '@chakra-ui/react';
+
+import { PostForm } from '@widgets/post-form/ui/post-form';
+
 export const AdminEditPost = () => {
-  return <div>AdminEditPost</div>;
+  return (
+    <VStack spacing={8} alignItems="flex-start">
+      <Text fontSize="2xl" fontWeight="bold">
+        Edit Post
+      </Text>
+      <PostForm />
+    </VStack>
+  );
 };
