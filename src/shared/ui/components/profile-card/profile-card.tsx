857388b144ab1b1252923503fd6e21fb468b0ef9
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-01-30 21:48:26 +0900

    Add: Avatar

--- src/shared/ui/components/profile-card/profile-card.tsx
+++ src/shared/ui/components/profile-card/profile-card.tsx
@@ -1,5 +1,20 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Image, Text } from '@chakra-ui/react';
+
+import { SharedImages } from '@shared/assets';
 
 export const ProfileCard = () => {
-  return <Box>ProfileCard</Box>;
+  return (
+    <Box>
+      <Box borderRadius="full" overflow="hidden" width="200px" height="200px">
+        <Image
+          src={SharedImages.avatar}
+          alt="avatar"
+          width="100%"
+          height="100%"
+          objectFit="cover"
+        />
+      </Box>
+      <Text>Tuan Le Hoang</Text>
+    </Box>
+  );
 };
