09b4696b3794b3ecec452b02fe447c9864c1d2ae
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-01-30 21:23:00 +0900

    Update: responsive for home

--- src/shared/layout/Homepage/Homepage-layout.tsx
+++ src/shared/layout/Homepage/Homepage-layout.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Container } from '@chakra-ui/react';
+import { Grid, GridItem, Container, Show, Divider } from '@chakra-ui/react';
 
 type HomepageLayoutProps = {
   sideBar: React.ReactNode;
@@ -8,9 +8,36 @@ type HomepageLayoutProps = {
 export const HomepageLayout = ({ sideBar, children }: HomepageLayoutProps) => {
   return (
     <Container maxW="container.xl">
-      <Grid templateColumns="repeat(3, 1fr)" gap={4}>
-        <GridItem colSpan={1}>{sideBar}</GridItem>
-        <GridItem colSpan={2}>{children}</GridItem>
+      <Grid
+        templateColumns={{
+          base: '1fr',
+          lg: 'repeat(24, 1fr)',
+        }}
+        gap={4}
+      >
+        <GridItem
+          colSpan={{
+            base: 'auto',
+            lg: 2,
+          }}
+        >
+          {sideBar}
+        </GridItem>
+        <Show below="lg">
+          <Divider
+            orientation="horizontal"
+            borderWidth="1px"
+            borderColor="gray.800"
+          />
+        </Show>
+        <GridItem
+          colSpan={{
+            base: 'auto',
+            lg: 20,
+          }}
+        >
+          {children}
+        </GridItem>
       </Grid>
     </Container>
   );
