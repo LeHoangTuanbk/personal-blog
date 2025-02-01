08231e39fc8c1b25caee023db7effca5745da132
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-02-01 20:39:25 +0900

    Update: Reduce description text length to 100 characters

--- src/shared/libs/create-description.ts
+++ src/shared/libs/create-description.ts
@@ -2,5 +2,5 @@ import { extractTextFromHTML } from './extract-text-from-html';
 
 export const createDescription = (html: string) => {
   const rawText = extractTextFromHTML(html);
-  return rawText.slice(0, 300);
+  return rawText.slice(0, 100);
 };
