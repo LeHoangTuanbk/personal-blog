9711bf97e89fcbdfdb18b2b1986d2e08e0e39c0e
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-02-01 17:13:56 +0900

    Update: labels posts

--- src/pages/home-label-posts/ui/home-label-posts.tsx
+++ src/pages/home-label-posts/ui/home-label-posts.tsx
@@ -1,3 +1,10 @@
+import { Box } from '@chakra-ui/react';
+import { useParams } from 'react-router-dom';
+
+import { LabelPostsContainer } from '@widgets/label-posts';
+
 export const HomeLabelPosts = () => {
-  return <div>HomeLabelPosts</div>;
+  const { label } = useParams();
+  if (!label) return <Box>Label not found</Box>;
+  return <LabelPostsContainer label={label} />;
 };
---
+++ src/widgets/label-posts/ui/label-posts-container.tsx
@@ -0,0 +1,15 @@
+import { Box, Spinner } from '@chakra-ui/react';
+
+import { useLabelPostsQuery } from '@widgets/label-posts/api/queries';
+import { LabelPosts } from '@widgets/label-posts/ui/label-posts';
+
+type LabelPostsContainerProps = {
+  label: string;
+};
+export const LabelPostsContainer = ({ label }: LabelPostsContainerProps) => {
+  const { data, isLoading, isError } = useLabelPostsQuery(label);
+  if (isLoading) return <Spinner />;
+  if (isError) return <Box>Error loading posts</Box>;
+  if (!data?.length) return <Box mt={10}>No posts found</Box>;
+  return <LabelPosts posts={data} />;
+};
